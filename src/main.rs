b7081f37931535b8f73f73835d2a59183207856d
kula <115645232@qq.com>
2018-11-18 10:57:47 +0800

    增加run调用入口

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,31 @@ extern crate clap;
 use std::io;
 use clap::{Arg, App, SubCommand};
 
+
+
+
+
 fn kernel() -> Result<String, io::Error> {
-    App::new("kunit")
+    let matches = App::new("kunit")
         .version("1.0")
         .about("kunit container")
         .author("kula")
+        .subcommand(
+            SubCommand::with_name("run")
+                .about("Run a kunit container"),
+        )
         .get_matches();
+    match matches.subcommand() {
+        ("run", Some(run_matches)) => {
+           println!("run kunit")
+        }
+        _ => println!("command not recognized"),
+    }
 
     let mut s = String::new();
     Ok(s)
 }
+
 
 fn main() {
     kernel();
