23dfab1191b2c76e3e540313d9846ed377f7927b
kula <115645232@qq.com>
2018-11-18 09:34:19 +0800

    增加 arg parse

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+extern crate clap;
+
+use std::io;
+use clap::{Arg, App, SubCommand};
+
+fn kernel() -> Result<String, io::Error> {
+    App::new("kunit")
+        .version("1.0")
+        .about("kunit container")
+        .author("kula")
+        .get_matches();
+
+    let mut s = String::new();
+    Ok(s)
+}
+
 fn main() {
+    kernel();
     println!("kunit start");
 }
+
